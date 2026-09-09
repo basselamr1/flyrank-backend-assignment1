8e99c2ae184667605f2b3f00c802ece365099556
basselamr1 <basselamr1@gmail.com>
2026-09-09 19:29:18 +0300

    Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return{"message": "Hello World!"}
+# @app.get('/')
+# async def root():
+#     return{"message": "Hello World!"}
+
+@app.get("/")
+async def get_api_description():
+    return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
+
+@app.get("/health")
+async def get_server_status():
+    return {"status": "ok"}
