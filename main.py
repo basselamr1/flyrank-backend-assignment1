bd96ab1f6075c33fd73f705551b37a6c2d0e44e3
basselamr1 <basselamr1@gmail.com>
2026-09-11 18:54:25 +0300

    stage 3: create with validation

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Task(BaseModel):
+    title:str
+    done: bool = False
+    
 db = [
     {"id": 0, "title": "Task 1", "done": True}, 
     {"id": 1, "title": "Task 2", "done": False}, 
     {"id": 2, "title": "Task 3", "done": False}, 
-    {"id": 3, "title": "Task 4", "done": False} 
+    {"id": 3, "title": "Task 4", "done": False}
     ]
-
-# @app.get('/')
-# async def root():
-#     return{"message": "Hello World!"}
 
 @app.get("/")
 async def get_api_description():
@@ -27,13 +28,29 @@ async def get_tasks():
 
 @app.get('/tasks/{id}')
 async def get_task(id):
-    if(int(id)>len(db)):
+    task = next((item for item in db if item["id"] == id), None)
+    if task is None:
         raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND,
-            detail = f"Item with id: {id} was not found."
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Item with id: {id} was not found."
         ) 
-    else:
-        return db[int(id)]
+    return task
 
+@app.post('/tasks', status_code=status.HTTP_201_CREATED)
+async def add_task(task: Task):
+    new_id = max((item['id'] for item in db), default = 0) +1
 
+    if(not task.title or task.title==""):
+         raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Task title can't be empty."
+        )
 
+    new_task = {
+        "id": new_id,
+        "title": task.title, 
+        "done": task.done
+    }
+
+    db.append(new_task)
+    return new_task, db
